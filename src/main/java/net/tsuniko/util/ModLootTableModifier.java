2567b03afdd64f268c062f894c0a3897f42d2491
Tsun1ko <cuzimmrdna@gmail.com>
2025-11-27 22:23:14 +0100

    fixed typo in variable name

--- src/main/java/net/tsuniko/util/ModLootTableModifier.java
+++ src/main/java/net/tsuniko/util/ModLootTableModifier.java
@@ -19,7 +19,7 @@ public class ModLootTableModifier {
     private static final RegistryKey<LootTable> JUNGLE_TEMPLE_ID = LootTables.JUNGLE_TEMPLE_CHEST;
     private static final RegistryKey<LootTable> SIMPLE_DUNGEON_ID = LootTables.SIMPLE_DUNGEON_CHEST;
     private static final RegistryKey<LootTable> MINESHAFT_ID = LootTables.ABANDONED_MINESHAFT_CHEST;
-    private static final RegistryKey<LootTable> RUINDED_PORTAL_ID = LootTables.RUINED_PORTAL_CHEST;
+    private static final RegistryKey<LootTable> RUINED_PORTAL_ID = LootTables.RUINED_PORTAL_CHEST;
     private static final RegistryKey<LootTable> TRAIL_RUINS_COMMON_ID = LootTables.TRAIL_RUINS_COMMON_ARCHAEOLOGY;
     private static final Identifier SKELETON_ID = Identifier.ofVanilla("entities/skeleton");
 
@@ -31,7 +31,7 @@ public class ModLootTableModifier {
                     JUNGLE_TEMPLE_ID.equals(registryKey) ||
                             SIMPLE_DUNGEON_ID.equals(registryKey) ||
                             MINESHAFT_ID.equals(registryKey) ||
-                            RUINDED_PORTAL_ID.equals(registryKey)
+                            RUINED_PORTAL_ID.equals(registryKey)
             )) {
                 builder.modifyPools(pool -> {
                     pool.with(
